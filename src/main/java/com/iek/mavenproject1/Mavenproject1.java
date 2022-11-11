178c200ce8e87971715418e564a6c8d4aa849d33
User <User@erg1-pc06>
2022-11-11 15:43:10 +0200

    initialize myproject

---
+++ src/main/java/com/iek/mavenproject1/Mavenproject1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.iek.mavenproject1;
+
+/**
+ *
+ * @author User
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int x=20;
+        int y=18;
+        if(x>y){
+            System.out.println("x is greater than y");
+        }
+    }
+}
